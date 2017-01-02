bcbd6f34eba76a0b93103a7c43137a61bd6853fd
Kevin Tran <kevin2005tran@gmail.com>
2017-01-01 18:05:15 -0800

    successfull completed art gallery section

--- starter-code/public/app.js
+++ starter-code/public/app.js
@@ -1,8 +1,37 @@
-console.log('app.js connected');
+$(document).ready(function(){
+  $('#guess-number-form').on('submit', function(event){
+    event.preventDefault();
+    $.ajax({
+      url: '/pick-a-number',
+      method: 'GET',
+      data: $('#guess-number-form').serialize(),
+      success: handleGuessSuccess,
+      error: handleError
+    });
+  });
 
-$(document).ready(function() {
-    console.log('DOM ready');
+  function handleGuessSuccess(guessResponse){
+    console.log(guessResponse);
+    $('#high-low-correct').html(guessResponse);
+  }
 
+  $('#target-number-form').on('submit', function(event){
+    event.preventDefault();
+    $.ajax({
+      url: '/pick-a-number',
+      method: 'POST',
+      data: $('#target-number-form').serialize(),
+      success: handleTargetChangeSuccess,
+      error: handleError
+    });
+  });
 
-    
+  function handleTargetChangeSuccess(targetResponse){
+    console.log(targetResponse);
+    $('#target-number-form')[0].reset();
+  }
+
+  function handleError(jqXHR, status, error){
+    console.log('error:', error);
+  }
 });
--- starter-code/server.js
+++ starter-code/server.js
@@ -1,54 +1,57 @@
-// SERVER-SIDE JAVASCRIPT
-// run npm install to install all required packages before starting server
-
 var express = require('express');
 var app = express();
-
+var bodyParser = require('body-parser');
 
 // MIDDLEWARE
 app.use(express.static('public'));
-
-var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Allow CORS:
-// not necessary since we'll be making requests from a js file
-  // that we are also serving (as static assets in public)
-// app.use(function(request, response, next) {
-//   response.header("Access-Control-Allow-Origin", "*");
-//   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
 // ROUTES
-// Root Route
-// app.get('/', function (request, response) {
-//   response.send('hello there!')
-// });
-
 app.get('/', function (request, response) {
-  response.sendFile('views/index.html' , {root : __dirname});
+  response.sendFile('views/index.html' , { root : __dirname});
 });
 
-var targetNum = 10;
+// Guessing Game
+var targetNumber = 10;
 
-// route /pickanumber number=5 console.log ('too low/ too high')
-app.get('/pickanumber', function(request, response){
-  var number = parseInt(request.query.number);
-
-  if (number === targetNum) {
-    response.send('nailed it!');
-  } else if (number < targetNum) {
-    response.send('Too low...');
-  } else if (number > targetNum) {
-    response.send('Too high!');
+app.get('/pick-a-number', function(request, response){
+  var num = parseInt(request.query.number);
+  if (num === targetNumber){
+    response.send('Nailed it!');
+  } else if (num > targetNumber){
+    response.send('Too High!');
+  } else if (num < targetNumber) {
+    response.send('Too Low');
+  } else {
+    response.send('something\'s wrong with that number');
   }
 });
 
+app.post('/pick-a-number', function(request, response){
+  targetNumber = parseInt(request.body.number);
+  response.send('updated number successfully!');
+});
 
+// Gallery
+app.get('/art-gallery', function (request, response) {
+  response.sendFile('views/art-gallery.html' , { root : __dirname});
+});
 
-// Gallery View Route
+var artworks = [];
 
+app.get('/artworks', function(request, response){
+  response.json(artworks);
+});
+
+app.post('/artworks', function(request, response){
+  var newArtwork = {
+    name: request.body.title,
+    description: request.body.description,
+    artist: request.body.artist
+  };
+  artworks.push(newArtwork);
+  response.json(artworks);
+});
 
 
 // SERVER START
